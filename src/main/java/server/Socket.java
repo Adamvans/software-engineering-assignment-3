6554c49502c0296ea56b23d2c174e4466140d3d6
Xar83 <40188397+Xar83@users.noreply.github.com>
2018-07-30 19:36:37 -0600

    Merge origin/master into Adam
    Conflicts:
    	src/main/java/com/aws/codestar/projecttemplates/Board.java

---
+++ src/main/java/server/Socket.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ *
+ * @author Ethgar
+ */
+@ServerEndpoint("/server")
+public class Socket {
+    SqlConnecter conn = new SqlConnecter();
+    
+    public Socket(){}
+    
+    @OnOpen
+    public void open(Session session) 
+    {  
+       conn.insertUser("testuser", "testpassword");
+    }
+    
+    @OnClose
+    public void close(Session session) 
+    {
+        
+    }
+    
+    @OnError
+    public void onError(Throwable error) 
+    {
+        Logger.getLogger(Socket.class.getName()).log(Level.SEVERE, null, error);
+    }
+    
+    @OnMessage
+    public void handleMessage(String message, Session session) throws IOException 
+    {
+        conn.insertUser(message, message);
+    }
+}
